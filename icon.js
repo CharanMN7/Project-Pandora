1a891be2d585d1fa811781c2bd45cf7677e6e5c2
CharanMN7_pc <maverickid247@gmail.com>
2022-08-14 20:49:44 +0530

    Wrote the header mark - I

---
+++ icon.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("cherry-icon");
+
+icon.onmouseover = function() {
+    icon.src = "css/images/icons/cherries-color.png";
+}
+
+icon.onmouseleave = function() {
+    icon.src = "css/images/icons/cherries-bw.png";
+}
+
