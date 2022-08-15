e52179be418d860dd9b37987ef2bf41e467cf916
CharanMN7_pc <maverickid247@gmail.com>
2022-08-15 19:04:20 +0530

    Finished adding 2 sections to the landing page

---
+++ index.js
@@ -0,0 +1,29 @@
+//Icon
+
+let icon = document.getElementById("cherry-icon");
+
+icon.onmouseover = function() {
+    icon.src = "css/images/icons/cherries-color.png";
+}
+
+icon.onmouseleave = function() {
+    icon.src = "css/images/icons/cherries-bw.png";
+}
+/*****************/
+
+//Menu
+
+let menuBtn = document.getElementById("menu-btn");
+let menu = document.getElementById("menu");
+let close = document.getElementById("close");
+
+menuBtn.onclick = function() {
+    menu.style.visibility = "visible";
+}
+
+close.onclick = function() {
+    menu.style.visibility = "hidden";
+}
+/***************/
+
+// 
