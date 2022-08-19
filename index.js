a298442b06886dddca1fecd3784836fc02149ba9
CharanMN7_pc <maverickid247@gmail.com>
2022-08-19 13:35:56 +0530

    Completed building the 2 product pages

--- index.js
+++ index.js
@@ -26,11 +26,38 @@ close.onclick = function() {
 }
 /***************/
 
-// Slider
+// Phone Slider
 
-let images = [
+let images_p = [
     "css/images/phone.png",
     "css/images/phone-curved.png"
+];
+
+let num_p = 0;
+
+function prev_p() {
+    var slider = document.getElementById("slider-p");
+    num_p--;
+    if(num_p < 0){
+        num_p = images_p.length-1;
+    }
+    slider.src = images_p[num_p];
+}
+
+function next_p() {
+    var slider = document.getElementById("slider-p");
+    num_p++;
+    if(num_p >= images_p.length){
+        num_p = 0;
+    }
+    slider.src = images_p[num_p];
+}
+
+// Laptop slider
+
+let images = [
+    "css/images/laptop-1.png",
+    "css/images/laptop-2.png"
 ];
 
 let num = 0;
